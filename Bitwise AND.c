fdc3caa94e80c4362f85da154aff726a2078079c
ft-akashh <akashjagtap0306@gmail.com>
2024-10-27 23:00:13 +0530

    Update Bitwise AND.c

--- Bitwise AND.c
+++ Bitwise AND.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main() {
-
-    int a = 12, b = 25;
-    printf("Output = %d", a & b);
-
-    return 0;
+int main() 
+{
+int a = 12, b = 25;
+printf("Output = %d", a & b);
+return 0;
 }
